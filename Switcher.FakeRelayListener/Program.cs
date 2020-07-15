638602ac62a4be4124f631abb5c0a6c4b19c3749
Stanislav Khavruk <stas_khavruk@outlook.com>
2020-07-15 23:03:21 +0300

    Added test project as fake UDP listener of relay

---
+++ Switcher.FakeRelayListener/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Switcher.FakeRelayListener
+{
+    internal class Program
+    {
+        private const int listenPort = 60001;
+
+        private static void StartListener()
+        {
+            Random random = new Random();
+            UdpClient listener = new UdpClient(listenPort);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+                    byte[] bytes = listener.Receive(ref groupEP);
+
+                    Console.WriteLine($"Received broadcast from {groupEP} :");
+                    foreach (var b in bytes)
+                    {
+                        Console.Write($"{b:X2} ");
+                    }
+                    Console.WriteLine();
+
+                    if (bytes[3] == 0)
+                    {
+                        listener.Connect(groupEP.Address.ToString(), groupEP.Port);
+                        byte[] response = { 0xFF, 0xAA, 00, 00, (byte)random.Next(1, 16) };
+                        listener.Send(response, response.Length);
+                    }
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+
+        public static void Main()
+        {
+            StartListener();
+        }
+    }
+}
