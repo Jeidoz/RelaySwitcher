7f38e2e5a11425126906414d629aad827dcf837e
Stanislav Khavruk <stas_khavruk@outlook.com>
2020-11-01 19:15:59 +0100

    Remove unused property

--- Switcher/Models/RelayButton.cs
+++ Switcher/Models/RelayButton.cs
@@ -5,19 +5,9 @@ namespace Switcher.Models
 {
     public sealed class RelayButton : BaseViewModel
     {
-        private bool _isVisible;
         private Style _style;
         private RelayLabel _relayLabel;
 
-        public bool IsVisible
-        {
-            get => _isVisible;
-            set
-            {
-                _isVisible = value;
-                OnPropertyChanged(nameof(IsVisible));
-            }
-        }
         public Style Style
         {
             get => _style;
