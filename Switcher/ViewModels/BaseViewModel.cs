2d419f5bbb398388c3a5a4cceeef0e0698ea6444
Stanislav Khavruk <stas_khavruk@outlook.com>
2020-11-01 19:16:19 +0100

    Remove unused virtual key word

--- Switcher/ViewModels/BaseViewModel.cs
+++ Switcher/ViewModels/BaseViewModel.cs
@@ -9,7 +9,7 @@ namespace Switcher.ViewModels
         public event PropertyChangedEventHandler PropertyChanged;
 
         [NotifyPropertyChangedInvocator]
-        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
